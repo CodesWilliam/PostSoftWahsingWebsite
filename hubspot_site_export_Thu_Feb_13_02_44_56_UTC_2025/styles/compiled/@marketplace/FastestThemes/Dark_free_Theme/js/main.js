282772e5bdf00aef27af0d56a77c5ad6570bb601
Idanord <33676867+Idanord@users.noreply.github.com>
2025-02-13 15:21:59 -0500

    Initial web Site
    code pulled from Hubspot

---
+++ hubspot_site_export_Thu_Feb_13_02_44_56_UTC_2025/styles/compiled/@marketplace/FastestThemes/Dark_free_Theme/js/main.js
@@ -0,0 +1,2 @@
+var mybutton=document.getElementById("myBtn");function scrollFunction(){document.body.scrollTop>20||document.documentElement.scrollTop>20?mybutton.style.display="block":mybutton.style.display="none"}function topFunction(){document.body.scrollTop=0,document.documentElement.scrollTop=0}window.onscroll=function(){scrollFunction()};
+//# sourceURL=https://46181478.fs1.hubspotusercontent-na1.net/hub/46181478/hub_generated/template_assets/175659988303/1734135186269/marketplace/FastestThemes/Dark_free_Theme/js/main.js
